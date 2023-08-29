4dacae4afa920f2a1b82e97a9942b9b8847d401f
Cairo <rcairor09@gmail.com>
2023-08-28 22:01:18 -0600

    correcciones

---
+++ TerminatorGym/Controllers/MembresiasController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TerminatorGym.Models;
+
+namespace TerminatorGym.Controllers
+{
+    public class MembresiasController : Controller
+    {
+        private readonly TerminatorGymContext _context;
+
+        public MembresiasController(TerminatorGymContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Membresias
+        public async Task<IActionResult> Index()
+        {
+            var terminatorGymContext = _context.Membresias.Include(m => m.Miembro);
+            return View(await terminatorGymContext.ToListAsync());
+        }
+
+        // GET: Membresias/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null || _context.Membresias == null)
+            {
+                return NotFound();
+            }
+
+            var membresia = await _context.Membresias
+                .Include(m => m.Miembro)
+                .FirstOrDefaultAsync(m => m.MembresiaId == id);
+            if (membresia == null)
+            {
+                return NotFound();
+            }
+
+            return View(membresia);
+        }
+
+        // GET: Membresias/Create
+        public IActionResult Create()
+        {
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "MiembroId", "MiembroId");
+            return View();
+        }
+
+        // POST: Membresias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MembresiaId,MiembroId,Precio,FechaInicio,FechaVencimiento,TipoMembresia")] Membresia membresia)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(membresia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "MiembroId", "MiembroId", membresia.MiembroId);
+            return View(membresia);
+        }
+
+        // GET: Membresias/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null || _context.Membresias == null)
+            {
+                return NotFound();
+            }
+
+            var membresia = await _context.Membresias.FindAsync(id);
+            if (membresia == null)
+            {
+                return NotFound();
+            }
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "MiembroId", "MiembroId", membresia.MiembroId);
+            return View(membresia);
+        }
+
+        // POST: Membresias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("MembresiaId,MiembroId,Precio,FechaInicio,FechaVencimiento,TipoMembresia")] Membresia membresia)
+        {
+            if (id != membresia.MembresiaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(membresia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MembresiaExists(membresia.MembresiaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MiembroId"] = new SelectList(_context.Miembros, "MiembroId", "MiembroId", membresia.MiembroId);
+            return View(membresia);
+        }
+
+        // GET: Membresias/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null || _context.Membresias == null)
+            {
+                return NotFound();
+            }
+
+            var membresia = await _context.Membresias
+                .Include(m => m.Miembro)
+                .FirstOrDefaultAsync(m => m.MembresiaId == id);
+            if (membresia == null)
+            {
+                return NotFound();
+            }
+
+            return View(membresia);
+        }
+
+        // POST: Membresias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            if (_context.Membresias == null)
+            {
+                return Problem("Entity set 'TerminatorGymContext.Membresias'  is null.");
+            }
+            var membresia = await _context.Membresias.FindAsync(id);
+            if (membresia != null)
+            {
+                _context.Membresias.Remove(membresia);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MembresiaExists(short id)
+        {
+          return (_context.Membresias?.Any(e => e.MembresiaId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TerminatorGym/Data/Migrations/20230829032036_migracion.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TerminatorGym.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Miembro",
+                columns: table => new
+                {
+                    MiembroId = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Miembro", x => x.MiembroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacto",
+                columns: table => new
+                {
+                    ContactoId = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MiembroId = table.Column<short>(type: "smallint", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacto", x => x.ContactoId);
+                    table.ForeignKey(
+                        name: "FK_Contacto_Miembro_MiembroId",
+                        column: x => x.MiembroId,
+                        principalTable: "Miembro",
+                        principalColumn: "MiembroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Membresia",
+                columns: table => new
+                {
+                    MembresiaId = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MiembroId = table.Column<short>(type: "smallint", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FechaInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaVencimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TipoMembresia = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membresia", x => x.MembresiaId);
+                    table.ForeignKey(
+                        name: "FK_Membresia_Miembro_MiembroId",
+                        column: x => x.MiembroId,
+                        principalTable: "Miembro",
+                        principalColumn: "MiembroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacto_MiembroId",
+                table: "Contacto",
+                column: "MiembroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membresia_MiembroId",
+                table: "Membresia",
+                column: "MiembroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacto");
+
+            migrationBuilder.DropTable(
+                name: "Membresia");
+
+            migrationBuilder.DropTable(
+                name: "Miembro");
+        }
+    }
+}
